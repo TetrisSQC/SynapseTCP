fbfd6b68a1d34a91e632fd94b2b9aab87c33de17
TetrisSQC <Hackbart@iMac.local>
2016-05-27 14:48:07 +0200

    bug fixes

--- Source/rtspsend.pas
+++ Source/rtspsend.pas
@@ -1,68 +1,9 @@
-{==============================================================================|
-| Project : Ararat Synapse                                       | 003.012.006 |
-|==============================================================================|
-| Content: HTTP client                                                         |
-|==============================================================================|
-| Copyright (c)1999-2011, Lukas Gebauer                                        |
-| All rights reserved.                                                         |
-|                                                                              |
-| Redistribution and use in source and binary forms, with or without           |
-| modification, are permitted provided that the following conditions are met:  |
-|                                                                              |
-| Redistributions of source code must retain the above copyright notice, this  |
-| list of conditions and the following disclaimer.                             |
-|                                                                              |
-| Redistributions in binary form must reproduce the above copyright notice,    |
-| this list of conditions and the following disclaimer in the documentation    |
-| and/or other materials provided with the distribution.                       |
-|                                                                              |
-| Neither the name of Lukas Gebauer nor the names of its contributors may      |
-| be used to endorse or promote products derived from this software without    |
-| specific prior written permission.                                           |
-|                                                                              |
-| THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"  |
-| AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE    |
-| IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE   |
-| ARE DISCLAIMED. IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE FOR  |
-| ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL       |
-| DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR   |
-| SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER   |
-| CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT           |
-| LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY    |
-| OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH  |
-| DAMAGE.                                                                      |
-|==============================================================================|
-| The Initial Developer of the Original Code is Lukas Gebauer (Czech Republic).|
-| Portions created by Lukas Gebauer are Copyright (c) 1999-2011.               |
-| All Rights Reserved.                                                         |
-|==============================================================================|
-| Contributor(s):                                                              |
-|==============================================================================|
-| History: see HISTORY.HTM from distribution package                           |
-|          (Found at URL: http://www.ararat.cz/synapse/)                       |
-|==============================================================================}
-
-{:@abstract(HTTP protocol client)
-
-Used RFC: RFC-1867, RFC-1947, RFC-2388, RFC-2616
-}
-
-{$IFDEF FPC}
-{.$MODE DELPHI}
-{$ENDIF}
 {$H+}
-//old Delphi does not have MSWINDOWS define.
 {$IFDEF WIN32}
 {$IFNDEF MSWINDOWS}
 {$DEFINE MSWINDOWS}
 {$ENDIF}
 {$ENDIF}
-
-{$IFDEF UNICODE}
-{$WARN IMPLICIT_STRING_CAST OFF}
-{$WARN IMPLICIT_STRING_CAST_LOSS OFF}
-{$ENDIF}
-
 unit rtspsend;
 
 interface
@@ -75,8 +16,6 @@ const
   cRTSPProtocol = '554';
 
 type
-
-  {:abstract(Implementation of HTTP protocol.)}
   TRTSPSend = class(TSynaClient)
   private
     FConnectionTimeOut: Integer;
@@ -94,7 +33,7 @@ type
     FResultCode: Integer;
     FResultString: string;
     FUserAgent: string;
-    FUploadSize: integer;
+    FUploadSize: Integer;
     function ReadUnknown: Boolean;
     function ReadIdentity(Size: Integer): Boolean;
     function PrepareHeaders: String;
@@ -104,13 +43,13 @@ type
     constructor Create;
     destructor Destroy; override;
 
-    {:Reset headers and document and Mimetype.}
+    { :Reset headers and document and Mimetype. }
     procedure Clear;
 
-    {:Decode ResultCode and ResultString from Value.}
+    { :Decode ResultCode and ResultString from Value. }
     procedure DecodeStatus(const Value: string);
 
-    {:Connects to host define in URL and access to resource defined in URL by
+    { :Connects to host define in URL and access to resource defined in URL by
      method. If Document is not empty, send it to server as part of HTTP request.
      Server response is in Document and headers. Connection may be authorised
      by username and password in URL. If you define proxy properties, connection
@@ -119,45 +58,46 @@ type
      If you use in URL 'https:' instead only 'http:', then your request is made
      by SSL/TLS connection (if you not specify port, then port 443 is used
      instead standard port 80). If you use SSL/TLS request and you have defined
-     HTTP proxy, then HTTP-tunnel mode is automaticly used .}
+     HTTP proxy, then HTTP-tunnel mode is automaticly used . }
     function RTSPMethod(const Method, URL: string; cSeq: Integer = -1): Boolean;
 
-    {:You can call this method from OnStatus event for break current data
-     transfer. (or from another thread.)}
+    { :You can call this method from OnStatus event for break current data
+     transfer. (or from another thread.) }
     procedure Abort;
 
-    property ConnectionTimeOut: Integer read FConnectionTimeOut write FConnectionTimeOut;
-    property CSeq: Integer read FCSeq write FCSeq;
-    {:Before HTTP operation you may define any non-standard headers for HTTP
+    property ConnectionTimeOut: Integer read FConnectionTimeOut
+      write FConnectionTimeOut;
+    property cSeq: Integer read FCSeq write FCSeq;
+    { :Before HTTP operation you may define any non-standard headers for HTTP
      request, except of: 'Expect: 100-continue', 'Content-Length', 'Content-Type',
      'Connection', 'Authorization', 'Proxy-Authorization' and 'Host' headers.
-     After HTTP operation contains full headers of returned document.}
+     After HTTP operation contains full headers of returned document. }
     property Headers: TStringList read FHeaders;
 
-    {:Stream with document to send (before request, or with document received
-     from HTTP server (after request).}
+    { :Stream with document to send (before request, or with document received
+     from HTTP server (after request). }
     property Document: TMemoryStream read FDocument;
     property PublicOptions: String read FPublicOptions;
 
-    {:Here you can specify custom User-Agent indentification. By default is
-     used: 'Mozilla/4.0 (compatible; Synapse)'}
+    { :Here you can specify custom User-Agent indentification. By default is
+     used: 'Mozilla/4.0 (compatible; Synapse)' }
     property UserAgent: string read FUserAgent write FUserAgent;
 
-    {:After successful @link(RTSPMethod) method contains result code of
-     operation.}
+    { :After successful @link(RTSPMethod) method contains result code of
+     operation. }
     property ResultCode: Integer read FResultCode;
 
-    {:After successful @link(RTSPMethod) method contains string after result code.}
+    { :After successful @link(RTSPMethod) method contains string after result code. }
     property ResultString: string read FResultString;
     property RTSPTimeOut: Integer read FRTSPTimeOut write FRTSPTimeOut;
     property Server: String read FServer;
     property Session: String read FSession write FSession;
 
-    {:if this value is not 0, then data upload pending. In this case you have
+    { :if this value is not 0, then data upload pending. In this case you have
      here total sice of uploaded data. It is good for draw upload progressbar
-     from OnStatus event.}
-    property UploadSize: integer read FUploadSize;
-    {:Socket object used for TCP/IP operation. Good for seting OnStatus hook, etc.}
+     from OnStatus event. }
+    property UploadSize: Integer read FUploadSize;
+    { :Socket object used for TCP/IP operation. Good for seting OnStatus hook, etc. }
     property Sock: TTCPBlockSocket read FSock;
     property StreamID: string read FStreamID write FStreamID;
     property Transport: string read FTransport;
@@ -177,7 +117,7 @@ begin
   FSock.SizeRecvBuffer := c64k;
   FSock.SizeSendBuffer := c64k;
   FTimeout := 20000;
-  FConnectionTimeOut:=0;
+  FConnectionTimeOut := 0;
   FTargetPort := cRTSPProtocol;
   FUserAgent := '';
   FUploadSize := 0;
@@ -187,7 +127,8 @@ end;
 destructor TRTSPSend.Destroy;
 begin
   FSock.Free;
-  if assigned(FDocument) then FDocument.Free;
+  if assigned(FDocument) then
+    FDocument.Free;
   FHeaders.Free;
   inherited Destroy;
 end;
@@ -195,7 +136,7 @@ end;
 procedure TRTSPSend.Clear;
 begin
   if assigned(FDocument) then
-   FDocument.Clear;
+    FDocument.Clear;
   FHeaders.Clear;
 end;
 
@@ -213,7 +154,7 @@ end;
 
 function TRTSPSend.PrepareHeaders: String;
 begin
-  Result := {$IFDEF UNICODE}TMarshal.AsAnsi{$ENDIF}(AdjustLineBreaks(FHeaders.Text, tlbsCRLF));
+  Result := string({$IFDEF UNICODE}TMarshal.AsAnsi{$ENDIF}(AdjustLineBreaks(FHeaders.Text, tlbsCRLF)));
 end;
 
 function TRTSPSend.InternalDoConnect: Boolean;
@@ -221,7 +162,7 @@ begin
   Result := False;
   FSock.CloseSocket;
   FSock.Bind(FIPInterface, cAnyPort);
-  FSock.ConnectionTimeout:=FConnectionTimeOut;
+  FSock.ConnectionTimeOut := FConnectionTimeOut;
   if FSock.LastError <> 0 then
     Exit;
   FSock.Connect(FTargetHost, FTargetPort);
@@ -240,183 +181,184 @@ begin
     Result := True;
 end;
 
-function TRTSPSend.RTSPMethod(const Method, URL: string; cSeq: Integer = -1): Boolean;
+function TRTSPSend.RTSPMethod(const Method, URL: string;
+  cSeq: Integer = -1): Boolean;
 var
   Sending, Receiving: Boolean;
   Size: Integer;
   Prot, User, Pass, Host, Port, Path, Para, URI: string;
   s: String;
   l: TStringList;
-  x: integer;
-  u,c : integer;
+  x: Integer;
+  u, c: Integer;
 begin
-  {initial values}
+  { initial values }
   Result := False;
   FResultCode := 500;
   FResultString := '';
   FUploadSize := 0;
-  FPublicOptions:='';
-  if cSeq=-1 then
+  FPublicOptions := '';
+  if cSeq = -1 then
     Inc(FCSeq)
   else
-    FCSeq:=cSeq;
+    FCSeq := cSeq;
   URI := ParseURL(URL, Prot, User, Pass, Host, Port, Path, Para);
   try
-  Sending := FDocument.Size > 0;
-  {Headers for Sending data}
-  if Sending then
-  begin
-    FHeaders.Insert(0, 'Content-Length: ' + IntToStr(FDocument.Size));
-  end;
+    Sending := FDocument.Size > 0;
+  { Headers for Sending data }
+    if Sending then
+    begin
+      FHeaders.Insert(0, 'Content-Length: ' + IntToStr(FDocument.Size));
+    end;
   { setting User-agent }
-  if FUserAgent <> '' then
-    FHeaders.Insert(0, 'User-Agent: ' + FUserAgent);
+    if FUserAgent <> '' then
+      FHeaders.Insert(0, 'User-Agent: ' + FUserAgent);
 
-  if URI = '/*' then
-    URI := '*';
-  if FSession<>'' then
-    FHeaders.Insert(0, 'Session: ' + fSession);
-  if FCSeq>0 then
-    FHeaders.Insert(0, 'CSeq: ' + IntToStr(FCSeq));
+    if URI = '/*' then
+      URI := '*';
+    if FSession <> '' then
+      FHeaders.Insert(0, 'Session: ' + FSession);
+    if FCSeq > 0 then
+      FHeaders.Insert(0, 'CSeq: ' + IntToStr(FCSeq));
 
-  FHeaders.Insert(0, UpperCase(Method) + ' ' + URL + ' RTSP/1.0');
+    FHeaders.Insert(0, UpperCase(Method) + ' ' + URL + ' RTSP/1.0');
 
-  FTargetHost := Host;
-  FTargetPort := Port;
-  if FHeaders[FHeaders.Count - 1] <> '' then
-    FHeaders.Add('');
-  //Log.write(LOG_DEBUG,Method,FHeaders.Text);
+    FTargetHost := Host;
+    FTargetPort := Port;
+    if FHeaders[FHeaders.Count - 1] <> '' then
+      FHeaders.Add('');
+
   { connect }
-  if not InternalConnect then
-  begin
-    Exit;
-  end;
+    if not InternalConnect then
+    begin
+      Exit;
+    end;
 
   { reading Status }
-  FDocument.Position := 0;
+    FDocument.Position := 0;
 
   { upload content }
-  if sending then
-  begin
-    if FDocument.Size >= c64k then
+    if Sending then
     begin
-      FSock.SendString(PrepareHeaders);
-      FUploadSize := FDocument.Size;
-      FSock.SendBuffer(FDocument.Memory, FDocument.Size);
+      if FDocument.Size >= c64k then
+      begin
+        FSock.SendString(PrepareHeaders);
+        FUploadSize := FDocument.Size;
+        FSock.SendBuffer(FDocument.Memory, FDocument.Size);
+      end
+      else
+      begin
+        s := PrepareHeaders + ReadStrFromStream(FDocument, FDocument.Size);
+        FUploadSize := Length(s);
+        FSock.SendString(s);
+      end;
     end
     else
     begin
-      s := PrepareHeaders + ReadStrFromStream(FDocument, FDocument.Size);
-      FUploadSize := Length(s);
-      FSock.SendString(s);
-    end;
-  end
-  else
-  begin
     { we not need to upload document, send headers only }
-    FSock.SendString(PrepareHeaders);
-  end;
-
-  if FSock.LastError <> 0 then
-    Exit;
-
-  Clear;
-  Size := -1;
-
-  repeat
-    repeat
-      s := FSock.RecvString(FTimeout);
-      if s <> '' then
-        Break;
-    until FSock.LastError <> 0;
-    if Pos('RTSP/', UpperCase(s)) = 1 then
-    begin
-      FHeaders.Add(s);
-      DecodeStatus(s);
-    end
-    else
-    begin
-      { old HTTP 0.9 and some buggy servers not send result }
-      s := s + CRLF;
-      WriteStrToStream(FDocument, s);
-      FResultCode := 0;
+      FSock.SendString(PrepareHeaders);
     end;
-  until (FSock.LastError <> 0) or (FResultCode <> 100);
 
-  { if need receive headers, receive and parse it }
-  if FHeaders.Count > 0 then
-  begin
-    l := TStringList.Create;
-    try
+    if FSock.LastError <> 0 then
+      Exit;
+
+    Clear;
+    Size := -1;
+
+    repeat
       repeat
         s := FSock.RecvString(FTimeout);
-        l.Add(s);
-        if s = '' then
+        if s <> '' then
           Break;
       until FSock.LastError <> 0;
-      x := 0;
-      while l.Count > x do
+      if Pos('RTSP/', UpperCase(s)) = 1 then
       begin
-        s := NormalizeHeader(l, x);
         FHeaders.Add(s);
-        u := Pos(':', s);
-        if u > 0 then
-        begin
-          repeat
-            Inc(u);
-          until (u > Length(s)) or (s[u{$IFDEF NEXTGEN}-1{$ENDIF}] <> ' ');
-          if (CompareString('CSeq:', s)) then
-          begin
-            FCSeq := StrToIntDef(Copy(s, u, Length(s)),0);
-          end
-          else if (CompareString('Session:', s)) then
-          begin
-            c:= Pos(';timeout=',s);
-            if c>0 then
-            begin
-              FRTSPTimeOut:=StrToIntDef(Copy(s, u + 9, Length(s)),10);
-              Delete(s,c,Length(s));
-            end;
-            FSession := Copy(s, u, Length(s))
-          end
-          else if (CompareString('Public:', s)) then
-          begin
-            FPublicOptions := Copy(s, u, Length(s))
-          end
-          else if (CompareString('Content-Length:', s)) then
-          begin
-            Size := StrToIntDef(Copy(s, u, Length(s)), -1);
-          end
-          else if (CompareString('com.ses.streamID:', s)) then
-          begin
-            FStreamID := Copy(s, u, Length(s));
-          end
-          else if (CompareString('Transport:', s)) then
-          begin
-            FTransport := Copy(s, u, Length(s));
-          end
-          else if (CompareString('Server:', s)) then
-          begin
-            FServer := Copy(s, u, Length(s));
-          end
-        end;
+        DecodeStatus(s);
+      end
+      else
+      begin
+      { old HTTP 0.9 and some buggy servers not send result }
+        s := s + CRLF;
+        WriteStrToStream(FDocument, s);
+        FResultCode := 0;
       end;
-    finally
-      l.free;
+    until (FSock.LastError <> 0) or (FResultCode <> 100);
+
+  { if need receive headers, receive and parse it }
+    if FHeaders.Count > 0 then
+    begin
+      l := TStringList.Create;
+      try
+        repeat
+          s := FSock.RecvString(FTimeout);
+          l.Add(s);
+          if s = '' then
+            Break;
+        until FSock.LastError <> 0;
+        x := 0;
+        while l.Count > x do
+        begin
+          s := NormalizeHeader(l, x);
+          FHeaders.Add(s);
+          u := Pos(':', s);
+          if u > 0 then
+          begin
+            repeat
+              Inc(u);
+            until (u > Length(s)) or (Copy(s, u, 1) <> ' ');
+            if (CompareString('CSeq:', s)) then
+            begin
+              FCSeq := StrToIntDef(Copy(s, u, Length(s)), 0);
+            end
+            else if (CompareString('Session:', s)) then
+            begin
+              c := Pos(';timeout=', s);
+              if c > 0 then
+              begin
+                FRTSPTimeOut := StrToIntDef(Copy(s, u + 9, Length(s)), 10);
+                Delete(s, c, Length(s));
+              end;
+              FSession := Copy(s, u, Length(s))
+            end
+            else if (CompareString('Public:', s)) then
+            begin
+              FPublicOptions := Copy(s, u, Length(s))
+            end
+            else if (CompareString('Content-Length:', s)) then
+            begin
+              Size := StrToIntDef(Copy(s, u, Length(s)), -1);
+            end
+            else if (CompareString('com.ses.streamID:', s)) then
+            begin
+              FStreamID := Copy(s, u, Length(s));
+            end
+            else if (CompareString('Transport:', s)) then
+            begin
+              FTransport := Copy(s, u, Length(s));
+            end
+            else if (CompareString('Server:', s)) then
+            begin
+              FServer := Copy(s, u, Length(s));
+            end
+          end;
+        end;
+      finally
+        l.Free;
+      end;
     end;
-  end;
 
-  Result := FSock.LastError = 0;
-  if not Result then
-    Exit;
+    Result := FSock.LastError = 0;
+    if not Result then
+      Exit;
 
-  {if need receive response body, read it}
-  Receiving := (FResultCode <> 204);
-  Receiving := Receiving and (FResultCode <> 304);
-  if Receiving and (Size>0) then
-    Result := ReadIdentity(Size);
+  { if need receive response body, read it }
+    Receiving := (FResultCode <> 204);
+    Receiving := Receiving and (FResultCode <> 304);
+    if Receiving and (Size > 0) then
+      Result := ReadIdentity(Size);
 
-  FDocument.Position := 0;
+    FDocument.Position := 0;
   except
     FSock.CloseSocket;
   end;
@@ -426,7 +368,7 @@ function TRTSPSend.ReadUnknown: Boolean;
 var
   s: string;
 begin
-  Result := false;
+  Result := False;
   repeat
     s := FSock.RecvPacket(FTimeout);
     if FSock.LastError = 0 then
@@ -434,7 +376,7 @@ begin
   until FSock.LastError <> 0;
   if FSock.LastError = WSAECONNRESET then
   begin
-    Result := true;
+    Result := True;
     FSock.ResetLastError;
   end;
 end;
@@ -448,7 +390,7 @@ begin
     Result := FSock.LastError = 0;
   end
   else
-    Result := true;
+    Result := True;
 end;
 
 procedure TRTSPSend.Abort;
@@ -457,4 +399,3 @@ begin
 end;
 
 end.
-
